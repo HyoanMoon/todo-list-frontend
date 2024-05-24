b87a3b95e14e61d937b696b7e2c503b8f47d8223
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-05-25 00:08:09 +0900

    modify design

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { MdOutlineCheckBox } from "react-icons/md";
+import { FaTrashAlt } from "react-icons/fa";
+
+
 
 const TodoItem = ({item,toggleTaskCompletion, deleteTask}) => {
   return (
@@ -9,8 +14,9 @@ const TodoItem = ({item,toggleTaskCompletion, deleteTask}) => {
           <div className="todo-content">{item.task}</div>
 
           <div>
-            <button className="button-delete" onClick={()=>deleteTask(item._id)}>삭제</button>
-            <button className="button-delete" onClick={() => toggleTaskCompletion(item._id)}> {item.isComplete ? `안끝남` : `끝남`}</button>
+          <button className="button-delete" onClick={() => toggleTaskCompletion(item._id)}> {item.isComplete ? <MdOutlineCheckBox /> : <MdCheckBoxOutlineBlank />}</button>
+            <button className="button-delete" onClick={()=>deleteTask(item._id)}><FaTrashAlt /></button>
+            
           </div>
         </div>
       </Col>
