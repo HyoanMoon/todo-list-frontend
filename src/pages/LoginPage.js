6909b8a1f2482c46bd4c8d785f4063a7fbf994b0
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-05-28 01:36:45 +0900

    implement login and register page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,29 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import api from "../utils/api";
+import { useNavigate } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const [email, setEmail] =useState('');
+  const [password, setPassword] =useState('');
+  const [error,setError] = useState('');  // error를 저장해야지 보여준다..?
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  const handleSubmit = async (event) =>{
+    event.preventDefault();
+    try{
+      const response = await api.post("/user/login", {email,password});
+        if(response.status === 200){
+          setUser(response.data.user)
+          sessionStorage.setItem("token",response.data.token);
+          api.defaults.headers["authorization"] = "Bearer" + response.data.token; //토큰 값을 헤더에 저장해준다 ==>  다시 공부!!!!!!!
+          setError("")
+          navigate('/');
+
+        }else{
+          throw new Error(response.message); 
+        }
+      
+    }catch(error){
+      setError(error.message);
+    }
+
+  }
+
+  
+
   return (
     <div className="display-center">
-      <Form className="login-box">
-        <h1>로그인</h1>
+      {error && <div className="error-message">{error}</div>}
+      <Form className="login-box" onSubmit={handleSubmit} > 
+      
+        <h1>Sign In</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(event)=>setEmail(event.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(event)=>setPassword(event.target.value)}/>
         </Form.Group>
         <div className="button-box">
           <Button type="submit" className="button-primary">
             Login
           </Button>
           <span>
-            계정이 없다면? <Link to="/register">회원가입 하기</Link>
+            New to Todo App? <Link to="/register">Sign up now</Link>
           </span>
         </div>
       </Form>
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,34 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
+import api from "../utils/api";
 
 const RegisterPage = () => {
+  const [name ,setName] =useState('');
+  const [email, setEmail] =useState('');
+  const [password, setPassword] =useState('');
+  const [secondPassword, setSecondPassword] = useState ('');
+  const [error,setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); //refresh 막아주기
+    try{
+      if(password!==secondPassword) {
+        throw new Error("password does not match!");
+      }
+      const response = await api.post("/user", {name,email,password});
+      if(response.status === 200){
+        navigate('/login');
+      }else{
+        throw new Error(response.data.error);
+      }
+
+    }catch(error){
+      setError(error.message);
+    }
+
+  };
+
   return (
     <div className="display-center">
-      <Form className="login-box">
-        <h1>회원가입</h1>
+      <Form className="login-box" onSubmit={handleSubmit}>
+        <h1>Create account</h1>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="string" placeholder="Name" />
+          <Form.Control type="string" placeholder="First and last name" onChange={(event)=>setName(event.target.value)}/> 
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(event)=>setEmail(event.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(event)=>setPassword(event.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>re-enter the password</Form.Label>
-          <Form.Control type="password" placeholder="re-enter the password" />
+          <Form.Label>Re-enter password</Form.Label> 
+          <Form.Control type="password" placeholder="Re-enter password" onChange={(event)=>setSecondPassword(event.target.value)}/>
+          {error && <div className="bold"> {error}</div>}
         </Form.Group>
 
         <Button className="button-primary" type="submit">
-          회원가입
+          Sign Up
         </Button>
       </Form>
     </div>
